ee5cb1db23afbd7845a49803085b9f176877cd41
Ethan EHRLER <ethan.ehrler@etu.univ-grenoble-alpes.fr>
2023-11-09 11:28:15 +0100

    alert

---
+++ js.js
@@ -0,0 +1,4 @@
+document.getElementById("submit-btn").addEventListener("click", function () {
+    alert("Question or suggestion submitted!");
+    document.querySelector("textarea").value = "";
+});
